d388a0685f8b7037d1d533ae21756749b7e1abf7
Rynay <feirs911@gmail.com>
2021-04-01 14:53:46 +0300

    simple builder implementation

--- src/components/Builder/Builder.js
+++ src/components/Builder/Builder.js
@@ -1,5 +1,23 @@
+import { useDispatch } from 'react-redux';
+import ingredients from '../../store/ingredients';
+import { addIngredientAC } from '../../store/actions/ingredientsActions';
+
 const Builder = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  return (
+    <section>
+      {ingredients.map((ing) => (
+        <h3
+          key={ing.id}
+          onClick={() => {
+            dispatch(addIngredientAC(ing.id));
+          }}
+        >
+          {ing.name}
+        </h3>
+      ))}
+    </section>
+  );
 };
 
 export default Builder;
--- src/store/reducers/ingredientsReducer.js
+++ src/store/reducers/ingredientsReducer.js
@@ -9,7 +9,7 @@ const initialState = [{ ...ingredients[0] }];
 export const ingredientsReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_INGREDIENT:
-      return [...state, ...ingredients[action.payload]];
+      return [...state, { ...ingredients[action.payload] }];
     case REMOVE_INGREDIENT:
       return state.filter((ing) => ing.id !== action.payload);
     default:
