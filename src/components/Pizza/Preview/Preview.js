45261b7214c853f01b8ebe4b72bfae357d8b8e0a
Rynay <feirs911@gmail.com>
2021-04-03 15:38:42 +0300

    try to improve performance

--- src/components/Pizza/Preview/Preview.js
+++ src/components/Pizza/Preview/Preview.js
@@ -2,28 +2,44 @@ import { useSelector } from 'react-redux';
 import s from './Preview.module.css';
 
 const Preview = () => {
-  const ingredients = useSelector((store) => store.ingredients);
+  const allIngredients = useSelector((store) => store.allIngredients);
   return (
     <section className={s.container}>
-      {ingredients
+      {allIngredients
         .filter((ing) => ing.type.includes('basic'))
         .map((ing) => (
           <img key={ing.id} src={ing.img} alt={ing.name} />
         ))}
-      {ingredients
+
+      {allIngredients
         .filter((ing) => ing.type.includes('sauce'))
         .map((ing) => (
-          <img key={ing.id} src={ing.img} alt={ing.name} />
+          <img
+            key={ing.id}
+            src={ing.img}
+            alt={ing.name}
+            style={{ display: `${ing.added ? 'block' : 'none'}` }}
+          />
         ))}
-      {ingredients
+      {allIngredients
         .filter((ing) => ing.type.includes('cheese'))
         .map((ing) => (
-          <img key={ing.id} src={ing.img} alt={ing.name} />
+          <img
+            key={ing.id}
+            src={ing.img}
+            alt={ing.name}
+            style={{ display: `${ing.added ? 'block' : 'none'}` }}
+          />
         ))}
-      {ingredients
+      {allIngredients
         .filter((ing) => ing.type.includes('topping'))
         .map((ing) => (
-          <img key={ing.id} src={ing.img} alt={ing.name} />
+          <img
+            key={ing.id}
+            src={ing.img}
+            alt={ing.name}
+            style={{ display: `${ing.added ? 'block' : 'none'}` }}
+          />
         ))}
     </section>
   );
