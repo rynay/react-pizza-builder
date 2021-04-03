bb0d44e4cc09743aa4f828ff9c55fbfaef4659a0
Rynay <feirs911@gmail.com>
2021-04-03 16:04:34 +0300

    fixed pizza preview + preload images

--- src/components/Pizza/Preview/Preview.js
+++ src/components/Pizza/Preview/Preview.js
@@ -1,7 +1,10 @@
 import { useSelector } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import s from './Preview.module.css';
 
-const Preview = () => {
+const Preview = ({ location }) => {
+  const page = location.pathname.slice(1);
+  const allIngredients = useSelector((store) => store.allIngredients);
   const ingredients = useSelector((store) => store.ingredients);
   return (
     <section className={s.container}>
@@ -10,23 +13,50 @@ const Preview = () => {
         .map((ing) => (
           <img key={ing.id} src={ing.img} alt={ing.name} />
         ))}
-      {ingredients
+      {(page === 'sauce' ? allIngredients : ingredients)
         .filter((ing) => ing.type.includes('sauce'))
         .map((ing) => (
-          <img key={ing.id} src={ing.img} alt={ing.name} />
+          <img
+            key={ing.id}
+            src={ing.img}
+            alt={ing.name}
+            style={{
+              display: ing.added ? 'block' : 'none',
+              zIndex:
+                ingredients[ingredients.length - 1].img === ing.img ? '1' : '0',
+            }}
+          />
         ))}
-      {ingredients
+      {(page === 'cheese' ? allIngredients : ingredients)
         .filter((ing) => ing.type.includes('cheese'))
         .map((ing) => (
-          <img key={ing.id} src={ing.img} alt={ing.name} />
+          <img
+            key={ing.id}
+            src={ing.img}
+            alt={ing.name}
+            style={{
+              display: ing.added ? 'block' : 'none',
+              zIndex:
+                ingredients[ingredients.length - 1].img === ing.img ? '2' : '1',
+            }}
+          />
         ))}
-      {ingredients
+      {(page === 'topping' ? allIngredients : ingredients)
         .filter((ing) => ing.type.includes('topping'))
         .map((ing) => (
-          <img key={ing.id} src={ing.img} alt={ing.name} />
+          <img
+            key={ing.id}
+            src={ing.img}
+            alt={ing.name}
+            style={{
+              display: ing.added ? 'block' : 'none',
+              zIndex:
+                ingredients[ingredients.length - 1].img === ing.img ? '3' : '2',
+            }}
+          />
         ))}
     </section>
   );
 };
 
-export default Preview;
+export default withRouter(Preview);
